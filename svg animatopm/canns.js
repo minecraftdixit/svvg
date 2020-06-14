9f474b471f1f1a6b94add37df2eb6bd7c7e2c30d
minecraftdixit <63745645+minecraftdixit@users.noreply.github.com>
2020-06-14 13:40:23 +0530

    Add files via upload

--- svg animatopm/canns.js
+++ svg animatopm/canns.js
@@ -1,3 +1,4 @@
+// audio:
 var x = document.getElementById("myAudio");
 
 function playAudio() {
@@ -9,11 +10,79 @@ function pauseAudio() {
   x.pause();
 }
 
+// teffect:
+
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-type');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #f11414}";
+  document.body.appendChild(css);
+};
+
+
+
+
+
+
+// sine wavw:
+
 
 let ctx,
-  amplitude = 50,
+  amplitude = 100,
   wavelength = .01,
-  frequency = 0.01,
+  frequency = .01,
   df = frequency,
   W = innerWidth,
   H = innerHeight;
@@ -31,14 +100,13 @@ const update = () => {
       Math.cos(amplitude / df)) * amplitude * Math.tan(df));
   }
 
-  ctx.strokeStyle = `red`;
+  ctx.strokeStyle = `rgb(238, 29, 15)`;
   ctx.stroke();
 
   ctx.beginPath();
 
 
 
-  ctx.fill();
   ctx.closePath();
 }
 
